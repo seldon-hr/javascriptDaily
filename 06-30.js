aba74c99339d5dcec72927f61f963c18af1d19c9
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-30 18:45:22 -0500

    Update 06-30.js

--- 06-30.js
+++ 06-30.js
@@ -5,3 +5,11 @@ const admin = { admin: true, ...user };
 console.log(admin); //🤔?
 
 // Respuesta Correcta: B) { admin: true, name: "Lydia", age: 21 }
+// El spread operator en este ejemplo se encarga de propagar el objeto user dentro del objeto admin.
+
+// Sin usar el spread operator tendríamos un objeto anidado:
+
+//   { admin: true, { name: "Lydia", age: 21 } }  
+
+
+// Justamente el spread operator se encarga de expandir user para evitar el anidamiento.
