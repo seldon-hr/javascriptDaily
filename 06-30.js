00809e075922b9c940ca00dd3c0763c8af89d2bb
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-30 18:44:48 -0500

    Update 06-30.js

--- 06-30.js
+++ 06-30.js
@@ -1,5 +1,7 @@
+//¿Qué imprime el siguiente código?
 const user = { name: 'Lydia', age: 21 };
 const admin = { admin: true, ...user };
 
 console.log(admin); //🤔?
 
+// Respuesta Correcta: B) { admin: true, name: "Lydia", age: 21 }
