ad2247768a4d2569555c8f9686fb88bfc906f58b
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-01 21:08:47 -0500

    Update 07-01.js

--- 07-01.js
+++ 07-01.js
@@ -8,3 +8,8 @@ const settings = {
 
 const data = JSON.stringify(settings, ["level", "health"]);
 console.log(data); //🤔?
+
+// Respuesta Correcta: A) "{"level":19, "health":90}"
+// JSON.stringify puede recibir un 2do parámetro opcional denominado replacer, puede ser una función o un arreglo, 
+//   y se encarga de hacer un filtro de las propiedades del objeto que deseamos convertir a string,
+//     en el ejemplo solo deseamos convertir las propiedades ["level", "health"], ignorando username.
