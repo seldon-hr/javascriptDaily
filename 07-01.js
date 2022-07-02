fa2ef6fc229ccfc8f4fd7209bbb595039db18c23
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-01 21:07:46 -0500

    Update and rename 07-01.j to 07-01.js

---
+++ 07-01.js
@@ -0,0 +1,10 @@
+// Explica este código JavaScript 😬
+
+const settings = {
+  username: "lydiahallie",
+  level: 19,
+  health: 90
+};
+
+const data = JSON.stringify(settings, ["level", "health"]);
+console.log(data); //🤔?
