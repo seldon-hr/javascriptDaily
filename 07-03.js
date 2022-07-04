a8ba2a772aab32d538cf961e681d3103b763fb7d
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-03 21:07:51 -0500

    Update 07-03.js

--- 07-03.js
+++ 07-03.js
@@ -1,1 +1,12 @@
+// Explica este código JavaScript 🧐
 
+const value = { number: 10 };
+
+const multiply = (x = { ...value }) => {
+  console.log((x.number *= 2));
+};
+
+multiply(); //🤔?
+multiply(); //🤔?
+multiply(value); //🤔?
+multiply(value); //🤔?
