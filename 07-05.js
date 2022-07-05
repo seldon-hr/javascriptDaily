25d6cd3b7918f1e51ad4c2bb63c28af4fb0cc154
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-05 15:26:17 -0500

    Update 07-05.js

--- 07-05.js
+++ 07-05.js
@@ -1,2 +1,9 @@
 // ¿Cuál es la salida del siguiente código?
 
+const one = true || {} || null;
+const two = null || 'hello' || '';
+const three = 0 || [] || true;
+console.log(one, two, three);
+
+// Operador 'OR' ||
+
