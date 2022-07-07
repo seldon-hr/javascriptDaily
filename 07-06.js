3e64e7d7172314841a0d406f348b11bc963d635b
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-06 19:17:14 -0500

    Update 07-06.js

--- 07-06.js
+++ 07-06.js
@@ -6,3 +6,21 @@ function makeMulti(x) {
 const five = makeMulti(5);
 const rta = five(6);
 console.log(rta);
+
+// Respuesta Correcta: C) 30
+// Aquí tenemos el escenario de un closure, un closure es:
+
+// - Una función que puede recibir un dato y lo almacena en su scope
+// - Retorna una función que ocupa ese dato previamente guardado
+
+// En este caso estamos haciendo una "fábrica" de multiplicadores.
+
+// // five es una función que tiene guardada en su scope la variable x con el valor de 5
+// const five = makeMulti(5);
+
+// // Ahora podemos pasarle a la función five cualquier valor que queramos multiplicar por 5
+// const rta = five(6);
+
+// // Podemos hacer más funciones multiplicadoras como:
+// const seven = makeMulti(7);
+// const two = makeMulti(2);
