c838a62d08c96bf89b33225defd81b115792c62a
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-09 23:52:45 -0500

    Update 07-09.js

--- 07-09.js
+++ 07-09.js
@@ -1,1 +1,17 @@
 
+Explica este código JavaScript
+
+function greeting() {
+  throw 'Hello world!';
+}
+
+function sayHi() {
+  try {
+    const data = greeting();
+    console.log('It worked!', data);
+  } catch (e) {
+    console.log('Oh no an error!', e);
+  }
+}
+
+sayHi();
