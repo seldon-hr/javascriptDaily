61c5c4951f6521ebc7038cfc312668694e14f57a
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-13 22:51:49 -0500

    Update 07-12.js

--- 07-12.js
+++ 07-12.js
@@ -1,1 +1,15 @@
+Explica este código JavaScript 😎
+Basiccristian-fernando-villca-gutie
+var status = '😎';
 
+setTimeout(() => {
+  const status = '😍';
+
+  const data = {
+    status: '🥑',
+    getStatus() {
+      return this.status;
+    },
+  };
+  console.log(data.getStatus());
+}, 0);
