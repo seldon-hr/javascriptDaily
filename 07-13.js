926f65e1ec4f1eb8423c7fc4f543fbc05de27d92
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-14 17:19:24 -0500

    Update 07-13.js

--- 07-13.js
+++ 07-13.js
@@ -1,1 +1,9 @@
-
+function checkAge(age) {
+  if (age < 18) {
+    const message = "Sorry, you're too young.";
+  } else {
+    const message = "Yay! You're old enough!";
+  }
+  return message;
+}
+console.log(checkAge(21)); //🤔?
