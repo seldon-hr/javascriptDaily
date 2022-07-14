69fd702d9d080bf1e68435023c6072144578c636
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-13 22:52:50 -0500

    Update 07-12.js

--- 07-12.js
+++ 07-12.js
@@ -13,3 +13,7 @@ setTimeout(() => {
   };
   console.log(data.getStatus());
 }, 0);
+
+// Respuesta Correcta: A) 🥑
+// Al llamar a getStatus debemos tener el cuenta el scope de las variables, recuerda que tanto let como const tienen scope de bloque,
+// }por ende buscara una variable status dentro del bloque de data y regresara el emoji de palta.
