7f4a9be1ca85b1fe93dc7e1762f5ed047da95e83
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-18 15:32:09 -0500

    Update 07-18.js

--- 07-18.js
+++ 07-18.js
@@ -1,1 +1,23 @@
+// Explica este código JavaScript 🤔
 
+function getItems(fruitList, ...args, favoriteFruit) {
+  return [...fruitList, ...args, favoriteFruit]
+}
+getItems(["banana", "apple"], "pear", "orange"); //🤔?
+
+// Respuesta Correcta: D) SyntaxError
+// Cuando vemos en la lista de parámetros de una función la sintaxis de tres puntos ... nos referimos a lo que se denomina un paramétro de tipo REST. 
+// En el cuerpo de la función este tipo de parámetro se trata como un arreglo pero siempre debe estar declarado al final de la lista de parámetros, 
+// caso contrario tendremos un error de sintaxis.
+
+// Si volvemos a escribir la función pero esta vez teniendo en cuenta lo anterior dicho:
+//   function getItems(fruitList, favoriteFruit, ...args) {
+
+//  return [...fruitList, ...args, favoriteFruit]
+
+//  }
+
+//  console.log(getItems(["banana", "apple"], "pear", "orange"));  
+
+
+// Obtemos por consola: ["banana", "apple", "orange", "pear"]
