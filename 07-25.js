5abc418e61a42174e3fa40e2043b28287681c31c
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-25 21:00:42 -0500

    Update 07-25.js

--- 07-25.js
+++ 07-25.js
@@ -1,1 +1,9 @@
+// ¿Cuál es el resultado del siguiente código?
 
+const fruits = [
+  { id: 1, name: 'Lime', quantity: 5 },
+  { id: 2, name: 'Apple', quantity: 30 },
+  { id: 3, name: 'Pineapple', quantity: 3 },
+];
+
+const count = fruits.some((fruit) => fruit.quantity > 5);
