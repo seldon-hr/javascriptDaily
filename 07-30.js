64ce0ec75723e798352ba8130ae8bc8e9e71ed5a
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-30 21:59:19 -0500

    Update 07-30.js

--- 07-30.js
+++ 07-30.js
@@ -17,3 +17,5 @@ compareMembers(person);
 // Los objetos se pasan por referencia. En el ejemplo, person1 y person2 apuntan a la misma dirección de memoria entonces 
 // la condición del if no se cumple y pasamos a imprimir 
 // They are the same!.
+
+// Dentro hac sentido que ambos objetos tomen el valor de 'person', ya que, ambos toman solo un parámetro que fue recibido al momento de ser ejecutado.
