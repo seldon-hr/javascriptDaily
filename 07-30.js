3679e3aba5f00e8a91c9348d37e4cc473aeccfae
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-07-30 21:56:41 -0500

    Update and rename 07-30.j to 07-30.js

---
+++ 07-30.js
@@ -0,0 +1,12 @@
+// Explica este código JavaScript 😬
+
+function compareMembers(person1, person2 = person) {
+  if (person1 !== person2) {
+    console.log('Not the same!');
+  } else {
+    console.log('They are the same!');
+  }
+}
+
+const person = { name: 'Lydia' };
+compareMembers(person);
