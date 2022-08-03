031178577d09dcd77a542ff2b0db963772b95808
Daniel Romero <daanmaro99june@gmail.com>
2022-08-03 10:43:42 -0500

    Temporal Dead Zone

--- August/08-03.js
+++ August/08-03.js
@@ -7,3 +7,28 @@ function getName() {
 }
 
 getName();
+
+/*
+El siguiente código manda un 'Reference Error', porque la variable a la cual
+quiere acceder no existe, aún no se ha declarado al momento de imprimirla.
+
+Es decir, el scope de let y const tes de bloque, solo se conocen dentro de sus
+bloques, *la función es un bloque.
+
+La función getName intenta imprimir por consola name antes de ser declarada, por
+hoisting el interprete de JS hará que name entre en lo que se denomina 
+'Temporal Dead Zone, una región donde la variable esta declarada pero no es
+posible acceder a ella.
+Así produciendo un ReferenceError
+
+
+Ahora si dentro de la función getName la variable name estuviera declarada con var:
+
+funcition getName() {
+    console.log(name);
+    var name = 'Sarah';
+}
+
+Por hoisting el resultado sería undefined puesto que la Temporal Dead Zone solo
+existe con variables declaradas con let y const.
+*/
