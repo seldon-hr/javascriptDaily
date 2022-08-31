da804820a0d5844cb5bb5075b08695e2c69d5461
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-08-31 11:31:33 -0500

    Salto de posiciones con ,
    , nos permite hacer salto de posiciones dentro de arreglos.

--- August/08-31.js
+++ August/08-31.js
@@ -1,6 +1,17 @@
 const names = ['Idaho', 'Gurney', 'Hawat', ...['Paul']];
 const [, , , miName] = names;
 console.log(miName['length']);
+//Output 4
+
+/*
+Cuando pretendemos hace una desestructuración de arreglos es súper importante tener en cuenta los índices del mismo. Usando
+la sintaxis de la , podemos saltar posiciones del arreglo hasta encontrar la propiedad que se debe obtener.
+
+En el ejemplo usamos 3 veces , por ello saltamos 3 posiciones del array de names para poder obtner (con spread operator) la
+cadena Paul del array anidado.
+
+Finalmente aplicamos el método length con sintaxis de corchete.
 
 
+*/
 
