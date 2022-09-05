e49611a7e1cb7d8807d139421728924fe7f1c250
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-09-05 16:51:25 -0500

    Update 09-05.js

--- September/09-05.js
+++ September/09-05.js
@@ -1,2 +1,10 @@
 const array = ['a', 'bb', 'ccc'];
 const rta = array.map((item) => item.length);
+/*
+El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
+
+.map() es inmutable, por lo tanto, no modifica el array original, sino que crea uno nuevo con la “transformación” aplicada. Además, mantienes el mismo length que el array original,
+te devuelve en el nuevo array la misma cantidad que el array que le aplicaste el método.
+
+
+*/
