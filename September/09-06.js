a27ca5d6350fe86ceea831c55be8413b9ce9f273
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-09-06 16:07:59 -0500

    Función sumatora de un array con reduce()

--- September/09-06.js
+++ September/09-06.js
@@ -1,2 +1,13 @@
 const array = [1, 1, 1, 1, 1];
 const rta = array.reduce((total, item) =>  total + item, 0);
+
+/*
+El método reduce() recorre el array y llama la fucnión reductora, almacena el valor del cálculo del array por la función en
+un acumulador.
+Un acumulador es una variable que se recuerda todas las iteraciones para almacenar los resultados acumulados de recorrer un array.
+Podemos usar esto para iterar a través del array, agregar el valor del elemento acumulador y obtener la suma del array.
+
+El código representado es un ejemplo de la implementación de una función reductora para sumar todos los elementos del array.
+
+
+*/
