01c0baef058fc5caace194d245b4b51f9ad3671f
Daniel Romero <daanmaro99june@gmail.com>
2022-09-12 15:19:17 -0500

    Using spread operator with strings

---
+++ September/09-12.js
@@ -0,0 +1,6 @@
+console.log([...'Array']);
+
+/* 
+Un string es un elemento iterable en JavaScript, por ende es posible usar el
+spreado operator directamente obteniendo la propagación de la cadena letra por letra.
+*/
