73692a9920e164ca81e93a542306c4be698250b2
Daniel Romero <daanmaro99june@gmail.com>
2022-09-13 13:35:30 -0500

    Set interval funnciton

---
+++ September/09-13.js
@@ -0,0 +1,5 @@
+setInterval(() => console.log('hi', 1000));
+
+/* 
+La función setInterval es una Web Api que recibe un intervalo en milisegundos, e imprime el cuerpo de la función en dicho intervalo.
+*/
