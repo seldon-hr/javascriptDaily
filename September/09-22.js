e393a698c4556a2cfe9c6fafe21e8bc8b5767558
Daniel Romero <daanmaro99june@gmail.com>
2022-09-22 11:52:12 -0500

    Global, local variables

--- September/09-22.js
+++ September/09-22.js
@@ -6,3 +6,12 @@ if (true) {
 }
 console.log(x + z);
 
+/* 
+Las variables declaradas con let y const tienen un contexto de bloque, esto significa que solo podrán ser
+accedidas dentro del bloque de llaves donde fueron declaradas, por ejemplo dentro de un bloque if o dentro
+de una función.
+
+Esta premisa se cumple siempre y cuando esten declaradas dentro de un bloque, si una variable esta fuera de todo
+bloque entonces se dice que es una variable global y por ende puede ser accedida desde cualquier parte del código.
+
+*/
