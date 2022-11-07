410e447b1b58871e6ddc6aa14e2190d9d1a31287
Daniel Romero <daanmaro99june@gmail.com>
2022-11-06 19:54:46 -0600

    Operaciones con bits

--- november/06-11.js
+++ november/06-11.js
@@ -1,3 +1,8 @@
 const bitwiseAND = (n1, n2) => n1 & n2;
 const bitwiseOR = (n1, n2) => n1 | n2;
 const bitwiseXOR = (n1, n2) => n1 ^ n2;
+
+/* 
+    Uso de operaciones con bits, en JavaScript
+    Operaicones de bits
+*/
