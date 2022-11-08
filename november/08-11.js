13da9f9e5d812d92ef607b1c25ec668d29569c35
Daniel Romero <daanmaro99june@gmail.com>
2022-11-08 13:58:48 -0600

    s

---
+++ november/08-11.js
@@ -0,0 +1,13 @@
+const nombre = 'Cristian';
+nombre = 'Fernando';
+console.log(nombre);
+
+const persona = {
+    id: 1,
+    nombre: 'Cristian',
+};
+
+persona.nombre = 'Fernando';
+console.log(persona.nombre);
+
+/*  */
