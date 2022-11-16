5d11097aa2413162fb8c292b605fc47feec97494
Daniel Romero <daanmaro99june@gmail.com>
2022-11-15 20:29:11 -0600

    isArray() for job interviews

--- november/15-11.js
+++ november/15-11.js
@@ -1,2 +1,13 @@
 const arr = [];
 console.log(Array.isArray(arr)); // ??
+
+/* 
+    Respuesta Correcta: A) true
+Una manera adecuada de comprobar que un arreglo es efectivamente un arreglo es usar el constructor Array con su método isArray.
+
+Como arr es un arreglo (vacío pero arreglo al fin), entonces regresamos true.
+
+Como los arreglos no son un tipo de dato per se en JavaScript, la mejor manera de comprobar si un arreglo es un arreglo es de esta manera.
+
+¿Te cuento un secreto? Esta pregunta es bastante frecuente en entrevistas laborales, pero shhh, no se lo digas a nadie 👀
+*/
