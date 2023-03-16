d3d5768915222683c69a39db37dea03684f64321
Daniel Romero <daanmaro99june@gmail.com>
2023-03-15 18:50:46 -0600

    Logic operations

--- 15-03.js
+++ 15-03.js
@@ -35,3 +35,55 @@ function circuitPower(voltage, current) {
   return power;
 }
 
+
+/* Bitwise Operations
+A decimal number can be represented as a sequence of bits. To illustrate:
+
+6 = 00000110
+23 = 00010111
+From the bitwise representation of numbers, we can calculate the bitwise AND, bitwise OR and bitwise XOR. Using the example above:
+
+bitwiseAND(6, 23) ➞ 00000110
+
+bitwiseOR(6, 23) ➞ 00010111
+
+bitwiseXOR(6, 23) ➞ 00010001
+Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+Examples
+bitwiseAND(7, 12) ➞ 4
+
+bitwiseOR(7, 12) ➞ 15
+
+bitwiseXOR(7, 12) ➞ 11
+Notes
+JavaScript has a useful function: toString(2), where you can see the binary representation of a decimal number.
+ */
+function bitwiseAND(n1, n2) {
+	result = n1 & n2;
+	return result;
+}
+
+function bitwiseOR(n1, n2) {
+		result = n1 | n2;
+		return result;
+}
+
+function bitwiseXOR(n1, n2) {
+		result = n1 ^ n2;
+		return result;
+}
+function bitwiseAND(n1, n2) {
+  result = n1 & n2;
+  return result;
+}
+​
+function bitwiseOR(n1, n2) {
+    result = n1 | n2;
+    return result;
+}
+​
+function bitwiseXOR(n1, n2) {
+    result = n1 ^ n2;
+    return result;
+}
