c208daca09c575bf5ec002823ea7839eb466de11
Daniel Romero <daanmaro99june@gmail.com>
2023-03-15 18:29:58 -0600

    Power Calculator

--- 15-03.js
+++ 15-03.js
@@ -17,3 +17,21 @@ function findPerimeter(length, width) {
   perimeter = (length*2) + (width*2);
   return perimeter;
 }
+
+/* Power Calculator
+Create a function that takes voltage and current and returns the calculated power.
+
+Examples
+circuitPower(230, 10) ➞ 2300
+
+circuitPower(110, 3) ➞ 330
+
+circuitPower(480, 20) ➞ 9600
+Notes
+Requires basic calculation of electrical circuits (see Resources for info). */
+
+function circuitPower(voltage, current) {
+  power = voltage * current;
+  return power;
+}
+
