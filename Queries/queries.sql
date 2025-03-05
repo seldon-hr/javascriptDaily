9e20e3db1093515d82a91a9ba64517c88a12fb46
Seldon <daanmaro99june@gmail.com>
2025-03-05 10:22:22 -0600

    Add the use of the new exercies, SQL

---
+++ Queries/queries.sql
@@ -0,0 +1,32 @@
+UPDATE COTIZACION SET ESTATUSFIRMA = 'Approved' WHERE FOLIO = 'sz0tqssbbywztdx70harzabjpeh2tahv';
+
+
+SELECT
+    /* ID,
+    FECHAREGISTRO, */
+    NOMBRE,
+   /*  DESCRIPCION,
+    ESACTIVO, */
+    RUTA
+FROM
+    CCCONSTANTES
+WHERE
+    NOMBRE = 'uso';
+
+
+
+/* Tomando la data de dos tablas. */
+SELECT
+    P.NOMBREINSTANCIA,
+    CM.*
+FROM
+    CONFIGURACIONMULTIAGENTES CM
+JOIN
+    PLANTILLA P ON CM.PLANTILLA_ID = P.ID
+WHERE
+    CM.PLANTILLA_ID = 2;
+
+/* Borrar todos los registros de la instancia de ANUALIZADA */
+DELETE FROM CONFIGURACIONMULTIAGENTES 
+WHERE PLANTILLA_ID = 2;
+    
