041d9725fb840ecfaa539f1699c9752ab0cc1dd6
Seldon <daanmaro99june@gmail.com>
2025-04-08 14:30:07 -0600

    Enhance transformMap function with detailed comments on map vs array performance and update usage example

--- 2/explainingMaps.js
+++ 2/explainingMaps.js
@@ -44,14 +44,39 @@ const scientificAuthors = [
     }
 ];
 
-function transformMap(list) {
-    
+/* Al manejar un mapa creamos una key (identificador único), la cual nos ayuda a que se 
+transforme en la identificación del objeto o dato que se va a mapear
+conforme al mapa.
+
+¿Por qué la diferencia entre mapa para operaciones?
+Esto quiere decir que al obtener datos de un mapa 'no crece' el número de tiempo
+que se va a llevar cabo en lo que busca dentro de estas operaciones.
+
+e.g.
+A diferencia de una lista (Array), esta hace la iteración hasta encontrar conforme a la búsqueda
+que se este haciendo. En caso ejemplo de .find.
+Por el contrario, con map, es búsqueda en formato de key. No realiza una iteración. Usa
+internamente una tabla hash para acceder directo a la key.
+
+Con la búsqueda de .get en un mapa, es tipo operación 0(1). es decir el tiempo es constante
+Mientras una lista, el tiempo crece conforme hay más elementos 0(n)
+
+✔️ Map → acceso directo por clave → rápido (O(1))
+❌ Array → búsqueda secuencial → más lento (O(n))
+*/
+
+
+function transformMap(list, key) {
+    // Creación de mapa
     let mapaAutores = new Map();
     list.forEach(author => {
+        // Creamos una llave de identificación conforme a una llave, la cual será 
+        // el identificador.
         const usernameKey = author.username;
+        // Agregamos el objeto conforme a su llave.
         mapaAutores.set(usernameKey, author);
     });
-    console.log(mapaAutores);
+    console.log(mapaAutores.get(key));
 }
 
-transformMap(scientificAuthors);
+transformMap(scientificAuthors, 'carlsagan');
