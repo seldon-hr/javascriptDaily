ce39ad699a55245be3e028e8881e8a8f985f4f95
Seldon <daanmaro99june@gmail.com>
2025-04-22 13:08:45 -0600

    feat: Add initial structure for listMultiAgentes in Vuex store with sample data

--- April/Agency/main.js
+++ April/Agency/main.js
@@ -15,7 +15,78 @@ const store = new Vuex.Store({
         { index: 1, key: "plazosPago", value: "" },
         { index: 2, key: "color", value: "" },
         { index: 3, key: "nombre", value: "" },
-    ]
+    ],
+    listMultiAgentes: [
+    {
+      id: 1,
+      condicionantes: [
+        {
+          key: "plazosPago",
+          source: "valueForm",
+          targetValue: 25,
+          isEqualAll: 0
+        },
+        /* {
+          key: "color",
+          source: "valueForm",
+          targetValue: "",
+          isEqualAll: 1
+        } */
+      ],
+      esMultipleCondicionante: false,
+      consecuentes: [
+        {
+          ccComando: 101,
+          source: "nombre",
+          value: 'Parthenope'
+        }
+      ],
+      esMultipleConsecuente: false,
+      auxValorComparacion: ""
+    }
+    /*  {
+      id: 2,
+      condicionantes: [
+        {
+          key: "nivel_luz",
+          source: "sensor_luz_ambiente",
+          targetValue: {
+            min: 100,
+            max: 500
+          },
+          isEqualAll: 0
+        }
+      ],
+      esMultipleCondicionante: false,
+      consecuentes: [
+        {
+          ccComando: 205,
+          source: "persiana_norte",
+          value: "SUBIR"
+        },
+        {
+          ccComando: 206,
+          source: "luz_escritorio",
+          value: 75
+        },
+        {
+            ccComando: 207,
+            source: "sistema_alarma",
+            value: { armado: true, modo: "nocturno" }
+        }
+      ],
+      esMultipleConsecuente: true,
+      auxValorComparacion: ""
+    },
+    {
+      id: 3,
+      condicionantes: [],
+      esMultipleCondicionante: true,
+      consecuentes: [],
+      esMultipleConsecuente: false,
+      auxValorComparacion: "modo_inicial"
+    } */
+]
   },
   mutations: {
     increment(state) {
@@ -26,6 +97,7 @@ const store = new Vuex.Store({
     updateListComponents({ state }, valor) {
         state.listComponentes = valor;
     }
+    
   }
 });
 
