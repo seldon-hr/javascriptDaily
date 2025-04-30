1fa8a872785f69b228777ba709663e4d3d24ea45
Seldon <daanmaro99june@gmail.com>
2025-04-30 11:56:52 -0600

    fix(MapaAgentes): Correct variable name from keyMapa to keysMapa for consistency

--- April/Agency/Class/MapaAgentes.js
+++ April/Agency/Class/MapaAgentes.js
@@ -1,7 +1,7 @@
 export default class MapaAgentes {
   constructor() {
     this.mapaGeneral = new Map();
-    this.keyMapa = new Map(); // cada alias apunta a una clave principal
+    this.keysMapa = new Map(); // cada alias apunta a una clave principal
   }
 
   /**
@@ -12,23 +12,23 @@ export default class MapaAgentes {
     /* Tomamos una key principal (mainKey) como llave del agente y esta se asigna como key y value(Agente) */
     const mainKey = keys[0];
     this.mapaGeneral.set(mainKey, value);
-    /* A nuestro keyMapa, agregamos cada una de las llaves que pueden apuntar a este agente(pero apuntan a la llave de mapaGeneral)
+    /* A nuestro keysMapa, agregamos cada una de las llaves que pueden apuntar a este agente(pero apuntan a la llave de mapaGeneral)
     Y asignamos diversas keys a esta key */
     for (const key of keys) {
-      this.keyMapa.set(key, mainKey);
+      this.keysMapa.set(key, mainKey);
     }
   }
 
 
   /**
    * Asigna un array de Keys a un value(Agente)
-   * Se pasa cualquier key y primero busca en el keyMapa, para saber que esta llave a quien puede apuntar.
+   * Se pasa cualquier key y primero busca en el keysMapa, para saber que esta llave a quien puede apuntar.
    * Y con esta nueva mainKey ahora si busca en el MapaGeneral. Y devuelve el Agente.
    * @param {String} key
    * @return {Object} Agente
    */
   get(key) {
-    const mainKey = this.keyMapa.get(key);
+    const mainKey = this.keysMapa.get(key);
     return this.mapaGeneral.get(mainKey);
   }
 }
