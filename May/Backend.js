c65f044e52bfe4aaa063135fee72d6d979e6684d
Seldon <daanmaro99june@gmail.com>
2025-05-13 20:30:23 -0600

    feat(Creación Backend versión): - Puntos a considerar cada vez que uno crea una versión de la app.

---
+++ May/Backend.js
@@ -0,0 +1,9 @@
+//Al momento de generar versiones con JAVA
+/* 
+    - Tener en cuenta en que apartado del workspace se esta trabajando.
+    - Si se esta agregando una variable que no corresponde. En apartado de Profiles.
+    - Asegurarse que en yarn build y en app.properties se vayan las env correspondientes.
+    - Manejo de una tag extra que se le puede agregar para saber que versión es QA, Desarrollo o Prouduction
+    - Iteración por medio de debugs a partir de que no viene algo. Desmenusar, y preguntar que se recibe en cada caso.
+
+*/
