7c226de0fe8df77836feb8d0ef9aaf83707dcb76
Seldon <daanmaro99june@gmail.com>
2025-05-27 14:11:57 -0600

    feat: Componentes conectados y actualizar desde el nieto, creando estructura de actualizar desde el hijo al padre.

--- April/Agency/App.vue
+++ April/Agency/App.vue
@@ -2,16 +2,24 @@
   <v-app>
     <v-container>
       <h1>Prototipo de Vue</h1>
-      <v-btn @click="cargarMultiAgentes"> Cargar MultiAgentes </v-btn>
+      <v-btn @click="cargarMultiAgentes"> Cargar Agentes </v-btn>
       <v-btn @click="refreshPage"> Refresh </v-btn>
       <v-divider />
       <v-row class="ma-3">
-        <custom-text-field
+        <!-- <custom-text-field
           v-for="(compound, index) in listComponentes"
           v-model="compound.value"
           :campo="compound"
           :key="index"
-        />
+        /> -->
+        <valores
+          v-for="(component, index) in listComponentes"
+          :key="index"
+          :type="component.key"
+          :value="component.value"
+          :md="12"
+        >
+        </valores>
       </v-row>
     </v-container>
   </v-app>
@@ -20,10 +28,12 @@
 <script>
 import { mapState, mapActions } from "vuex";
 import CustomTextField from "./components/CustomTextField.vue";
+import Valores from "./components/Valores.vue";
 export default {
   name: "App",
   components: {
     CustomTextField,
+    Valores,
   },
   computed: {
     ...mapState({
---
+++ April/Agency/components/Valores.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-col :md="md" class="ma-2 indigo lighten-2">
+    <v-row>
+      <v-col>
+        <v-autocomplete
+          v-model="localType"
+          :items="items"
+          outlined
+          label="Tipo"
+        />
+      </v-col>
+      <v-col>
+        <v-text-field v-model="localValue" label="Valor" outlined clearable />
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+<script>
+export default {
+  name: "Valores",
+  data() {
+    return {
+      items: [{ text: "Item 1" }, { text: "Item 2" }, { text: "Item 3" }],
+    };
+  },
+  props: {
+    md: { type: Number, String, default: 4 },
+    type: { type: String, default: "text" },
+    value: { type: [String, Number, Boolean, Object, Array], default: "" },
+  },
+  computed: {
+    localType: {
+      get() {
+        return this.type;
+      },
+      set(value) {
+        this.emit({ type: type, val: value });
+      },
+    },
+    localValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.emit({ type: type, val: value });
+      },
+    },
+  },
+};
+</script>
