52813a6308ceccf0c51d8663a81df62058625e9a
Seldon <daanmaro99june@gmail.com>
2025-06-26 12:51:56 -0600

    fix(ParentChild): FreeRecall sobre la comunicación entre el padre y el hijo con mandar información al hijo (:props), además de recibir información del hijo(@events)

---
+++ April/Agency/components/ChildComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-autocomplete
+    v-model="valorVModel"
+    :return-object="true"
+    item-text="test"
+    :items="items"
+    label="Listo para test:"
+  />
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { test: "Intercontinental Missil", valueTest: "damage done" },
+        { test: "Nuclear Bomb", valueTest: "catasthrophic" },
+        { test: "Hydrogen Bomb", valueTest: "doomsday" },
+      ],
+    };
+  },
+  props: {
+    value: { type: [String, Object] },
+  },
+  computed: {
+    valorVModel: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit("set:value", val);
+      },
+    },
+  },
+};
+</script>
