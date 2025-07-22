67da22c79d48818037ea7225d17897599092c2a5
Seldon <daanmaro99june@gmail.com>
2025-07-22 16:41:46 -0600

    feat(anagrams): Redefinir y hacer uso por mapas y su relación con los valores.

--- nu/02anagrams.js
+++ nu/02anagrams.js
@@ -18,88 +18,22 @@ let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
  */
 
 var groupAnagrams = function (strs) {
-
-    let mapaGrafo = transformarLettersToMapRelation(strs);
+    let arrayFinal = []
+    let mapaVisited = new Map();
+    strs.forEach(word => {
+        let key = word.split('').sort().join('');
+        if (mapaVisited.has(key)) {
+            mapaVisited.get(key).push(word);
+           
+        } else {
+            mapaVisited.set(key, [word]);
+        }
+    });
     
-    console.debug(mapaGrafo);
+    arrayFinal = Array.from(mapaVisited.values());
+
+    return arrayFinal;
 };
 
 console.debug(groupAnagrams(strs));
 
-
-function transformarLettersToMapRelation (strs) {
-    let mapaLetras = new Map();
-    strs.forEach(word => {
-        let wordPerLetters = word.split("");
-
-        wordPerLetters.forEach(letter => {
-
-            if (mapaLetras.has(letter)) {
-                let mapaLetrasAux = mapaLetras.get(letter);
-                
-                let existeEnMapaPalabra = mapaLetrasAux.some(w => w === word);
-                if (!existeEnMapaPalabra) {
-                    mapaLetrasAux.push(word);
-                }
-                mapaLetras.set(letter, mapaLetrasAux);
-            } else {
-                //Agregar por primera ocasión.
-                mapaLetras.set(letter, [word]);
-            }
-        });
-    });
-    return mapaLetras;
-}
-
-
-/* Ahora parte media
-Recibo esto:
-Map(5) {
-  'e' => [ 'eat', 'tea', 'ate' ],
-  'a' => [ 'eat', 'tea', 'tan', 'ate', 'nat', 'bat' ],
-  't' => [ 'eat', 'tea', 'tan', 'ate', 'nat', 'bat' ],
-  'n' => [ 'tan', 'nat' ],
-  'b' => [ 'bat' ]
-}
-
-Salida
-let resultBAT = [[arrayB], [arrayA], [arrayT]];
-*/
-
-
-
-let arrayB = ['bat'];
-let arrayA = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
-let arrayT = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
-let resultBAT = [[arrayB], [arrayA], [arrayT]];
-/* console.debug(resultBAT) */
-
-
-
-
-
-
-
-
-function returnFullArray(resultBAT) {
-    let arrayMaster = [];
-
-
-    resultBAT.forEach(lista => {
-        //Acceder a cada uno de la lista.
-        let listaF = lista[0];
-        listaF.forEach(word => {
-            console.debug('Palabra a checar', word)
-            let seEncuentraTodos = resultBAT.every(list => list[0].some(w => w === word));
-            console.debug(`Resultado de consulta de: ${word}`, seEncuentraTodos);
-            if (seEncuentraTodos) {
-                const seEncuentraEnLista = arrayMaster.some(wF => wF === word);
-                if (!seEncuentraEnLista) {
-                    arrayMaster.push(word);
-                }
-            }
-        })
-    });
-    return arrayMaster;
-}
-console.debug('here we are:', returnFullArray(resultBAT));
