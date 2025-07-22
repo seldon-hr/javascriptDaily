284bd4ee1de710d3c45cc421500e64b5844a21af
Seldon <daanmaro99june@gmail.com>
2025-07-22 13:17:44 -0600

    feat(anagrams): Transformar strings en mapa en relación letras conforme a su palabra que contiene.

--- nu/02anagrams.js
+++ nu/02anagrams.js
@@ -11,30 +11,60 @@ Explanation:
     The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
     The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other. */
 
-let stringsArray = ["eat", "tea", "tan", "ate", "nat", "bat"];
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 
 var groupAnagrams = function (strs) {
-    let conjuntoMayor = [];
-    for (let index = 0; index < strs.length; index++) {
-        const word = strs[index];
 
-        let cojuntoPerWord = [];
-        let wordAsArray = (word.split(""));
-
-
-
-
-        
-       conjuntoMayor.push(cojuntoPerWord);
-    }
-    return conjuntoMayor;
+    let mapaGrafo = transformarLettersToMapRelation(strs);
+    
+    console.debug(mapaGrafo);
 };
 
-console.debug(groupAnagrams(stringsArray));
+console.debug(groupAnagrams(strs));
+
+
+function transformarLettersToMapRelation (strs) {
+    let mapaLetras = new Map();
+    strs.forEach(word => {
+        let wordPerLetters = word.split("");
+
+        wordPerLetters.forEach(letter => {
+
+            if (mapaLetras.has(letter)) {
+                let mapaLetrasAux = mapaLetras.get(letter);
+                
+                let existeEnMapaPalabra = mapaLetrasAux.some(w => w === word);
+                if (!existeEnMapaPalabra) {
+                    mapaLetrasAux.push(word);
+                }
+                mapaLetras.set(letter, mapaLetrasAux);
+            } else {
+                //Agregar por primera ocasión.
+                mapaLetras.set(letter, [word]);
+            }
+        });
+    });
+    return mapaLetras;
+}
+
+
+/* Ahora parte media
+Recibo esto:
+Map(5) {
+  'e' => [ 'eat', 'tea', 'ate' ],
+  'a' => [ 'eat', 'tea', 'tan', 'ate', 'nat', 'bat' ],
+  't' => [ 'eat', 'tea', 'tan', 'ate', 'nat', 'bat' ],
+  'n' => [ 'tan', 'nat' ],
+  'b' => [ 'bat' ]
+}
+
+Salida
+let resultBAT = [[arrayB], [arrayA], [arrayT]];
+*/
 
 
 
@@ -44,12 +74,19 @@ let arrayT = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
 let resultBAT = [[arrayB], [arrayA], [arrayT]];
 /* console.debug(resultBAT) */
 
+
+
+
+
+
+
+
 function returnFullArray(resultBAT) {
     let arrayMaster = [];
 
 
     resultBAT.forEach(lista => {
-
+        //Acceder a cada uno de la lista.
         let listaF = lista[0];
         listaF.forEach(word => {
             console.debug('Palabra a checar', word)
