775667716ca0bb3a6c0646a315958dcfe82c975a
Seldon <daanmaro99june@gmail.com>
2025-07-28 10:08:29 -0600

    fix(linke-list): Recorrer una lista linekd con recursividad.

--- nu/05validAnagram.js
+++ nu/05validAnagram.js
@@ -17,5 +17,64 @@ var isAnagram = function (s, t) {
     return s === t
 };
 
-console.debug(isAnagram('anagram', 'nagaram'));
-console.debug(isAnagram('rat', 'car'));
+/* console.debug(isAnagram('anagram', 'nagaram'));
+console.debug(isAnagram('rat', 'car')); */
+
+/* Understanding singly-linked lists */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    //Método push
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+}
+
+let listaLinked = new SinglyLinkedList();
+listaLinked.push(2);
+listaLinked.push(4);
+listaLinked.push(3);
+console.debug(listaLinked);
+
+//Parece que si quisiera logs de todos sus productos, entonces tengo que hacer recursividad
+// logging the singlyLinkedList
+//Ejemplo con una linked list que si realmente tiene head, como principio.
+let index = 0;
+let lista = [];0
+function printNode(node) {
+    console.debug(`Node ${index}, Valor: ${node.val}`)
+    index++
+    lista.push(node.val);
+    if (node.next) {
+        printNode(node.next);
+    }
+}
+
+printNode(listaLinked.head);
+console.debug(lista);
+
+
+
+
