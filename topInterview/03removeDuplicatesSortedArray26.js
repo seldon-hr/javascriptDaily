6139beb33d7e6cedeec222dd2933c6a628344c16
Seldon <daanmaro99june@gmail.com>
2025-07-31 14:29:24 -0600

    docs(removeDuplicates): Improve comments for clarity on duplicate removal logic

--- topInterview/03removeDuplicatesSortedArray26.js
+++ topInterview/03removeDuplicatesSortedArray26.js
@@ -4,7 +4,13 @@
  */
 var removeDuplicates = function(nums) {
     let index = 0;
-    
+    /* Dentro de nuestro array lo recorremos por posiciones, para poder comparar cada uno
+    de sus valores, de forma que necesitamos dos apuntadores
+    index, el primer valor
+    secondIndex siempre apuntar a index + 1. Y reiniciar por cada nuevo valor que apunte index.
+    Cuando coinciden los valores, lo eliminamos, pero no incrementamos, porque usamos splice y este recorre el valor eliminado.
+    Entonces cuando no coincide, entonces si incrementamos secondIndex.
+    */
     while (index < nums.length) {
         let secondIndex = index + 1;
         while (secondIndex < nums.length) {
