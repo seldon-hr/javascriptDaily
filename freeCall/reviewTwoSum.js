7c5cdae7987a8fdd6eaa1715d0a789bebce38064
Seldon <daanmaro99june@gmail.com>
2025-08-07 10:24:14 -0600

    feat(reviewTwoSum): Implement single loop solution for two-sum problem

---
+++ freeCall/reviewTwoSum.js
@@ -0,0 +1,17 @@
+/* Solución free recall, donde lo resolví a diferencia de la primera vez.
+    aquí tenemos el uso de un solo loop, a diferencia de haber usado
+    dos ciclcos anidados. */
+/* var twoSum = function (nums, target) {
+    let index = 0;
+    while (index < nums.length) {
+        let secondIndex = index + 1;
+        if (nums[index] + nums[secondIndex] === target) {
+            return [index, secondIndex];
+        }
+        secondIndex++
+        index++
+    }
+}
+ */
+
+
