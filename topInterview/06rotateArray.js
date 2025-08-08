fa454a18f74fa9244de4988120ed6955763ad04e
Seldon <daanmaro99june@gmail.com>
2025-08-08 13:24:01 -0600

    fix(rotateArray): Add detailed comments explaining the normalization of rotations

--- topInterview/06rotateArray.js
+++ topInterview/06rotateArray.js
@@ -4,6 +4,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
+    /* Tenemos que k es el número de rotaciones que va a tener un array que avanza.
+        en este ejemplo debajo es k = 3, ¿Qué siginfica normalizar?
+        Esto es aquello para poder reducir el número de vueltas innecesarias si esta se teniendo en un inicio.
+        Sí tuvieramos que k = 10, y no normalizamos, esto significa que tenemos que avanzar 10 veces, pero nuestro array 
+        es 6, esto significa que no tenemos que rotar 10, porque es dar vueltas de más, 
+        la normalización encuentra el valor según el que es k = 3 % 6, conforme al valor de length del array.
+        Esto reduce el número de vueltas que uno tiene que dar.
+        Es como si decimo a alguien gira 360°, entonces, por esto no debe girar, porque equivale a no girar.
+        Así si dicen 720°, es lo mismo de nuevo. Entonces, este enfoque con rotar. 
+    */
     k = k % nums.length; //normalizar 
 
     function reverse(index, indexDecrase) {
