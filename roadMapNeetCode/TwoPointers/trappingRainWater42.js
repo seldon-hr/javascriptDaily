b952c0138cf9b5e6e0306a90bd6f7647acc6977f
Seldon <daanmaro99june@gmail.com>
2025-08-27 13:45:30 -0600

    feat(trappingRainWater): Refactor trap function for improved logic and clarity

--- roadMapNeetCode/TwoPointers/trappingRainWater42.js
+++ roadMapNeetCode/TwoPointers/trappingRainWater42.js
@@ -1,9 +1,9 @@
-let heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+let heights = [4, 1, 2, 1, 1]
 /**
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
+/* var trap = function(height) {
     let i = 0;
     let totalArea = 0;
     while (i < height.length) {
@@ -26,5 +26,68 @@ var trap = function(height) {
     }
     return totalArea;
 };
+ */
+/**
+ * Second try
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let maxIzquierda = [];
+    let maxDerecha = [];
+    
+    //Max altura izquierda
+    let maxIzq = height[0];
+    for (let i = 0; i < height.length; i++) {
+        const element = height[i];
+        if (maxIzq > element) {
+            maxIzquierda.push(maxIzq);
+        } else {
+            maxIzquierda.push(element);
+            maxIzq = element;
+        }
+    }
+
+    //Max altura right
+    let maxDer = height[height.length - 1];
+    for (let i = height.length - 1; i >= 0; i--) {
+        const element = height[i];
+        if (maxDer > element) {
+            maxDerecha[i] = maxDer
+        } else {
+            maxDerecha[i] = element
+            maxDer = element;
+        }
+    }
+    
+
+    // Calcular áreas
+    let totalArea = 0;
+    let index = 0;
+    while (index < height.length) {
+        
+        let alturaIzquierda = maxIzquierda[index];
+        let alturaDerecha = maxDerecha[index];
+        let alturaActual = height[index];
+
+        totalArea += Math.min(alturaIzquierda, alturaDerecha) - alturaActual; 
+
+        index += 1
+    }
+
+    return totalArea
+    
+};
+
+
+
+
+
+
+
+
+
+
+
 
 console.debug(trap(heights));
