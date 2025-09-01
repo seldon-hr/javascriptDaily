9f453e3cd749c652a104f1dcf47562d1d4a42737
Seldon <daanmaro99june@gmail.com>
2025-09-01 13:17:15 -0600

    Comprender antes de saber en código

--- roadMapNeetCode/TwoPointers/duplicateZeroes1089.js
+++ roadMapNeetCode/TwoPointers/duplicateZeroes1089.js
@@ -2,18 +2,45 @@
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
+
+/* 
+* SKETCH, boceto en proceso. Aún no comprendo este ejercicio.
+Comprender antes de resolver en código.
+ */
 var duplicateZeros = function(arr) {
-    let i = 1; //Empecemos uno adelante, vamos a comparar con el previo
+    let index = 1; //Empecemos uno adelante, vamos a comparar con el previo
     let counter = 0;
-    while (i < arr.length) {
-        if (arr[i] !== 0) {
-            i += 1;
+    while (index < arr.length) {
+        if (arr[index] !== 0) {
+            index += 1;
             continue
         } else {
             counter += 1
         }
-        i += 1;        
+        index += 1;        
     }
+
+    let i = arr.length;
+    let j = arr.length + counter; //índice teórico
+    while (i > 0) {
+        if (arr[i] === 0) {
+            if (j > arr.length) {
+                j -= 1
+                continue
+            } else {
+                //Mover ambos
+                i -= 1
+                j -= 1
+            }
+        } else {
+            
+        }
+
+
+        i += 1
+    }
+
+
 
     return counter;
 };
