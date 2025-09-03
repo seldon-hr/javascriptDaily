de2accb7dd8ad154fa4149840f728eb3582b0709
Seldon <daanmaro99june@gmail.com>
2025-09-03 09:12:49 -0600

    fix(trapWate): Understand the use of total += maxLado - heights[lado]

--- freeCall/reviewTrappingRainWater.js
+++ freeCall/reviewTrappingRainWater.js
@@ -10,13 +10,20 @@ var trapWater = function (heights) {
     while (left < right) {
 
         if (heights[left] <= heights[right]) {
+            /* Al momento de escoger entre las dos alturas nos garantiza que en esta posición
+            maxLeft es menor que maxRight, por eso, ya no es necesario hacer esto
+            Math.min(maxLeft, maxRight), esto ya se corrobora con que es el  menor de ambos.
+            Esto evita esta operación redundante, pero no afecta en la complejidad.
+            */
             //Ajustar y tomar el valor inmediato, ¿por qué el máximo?
-            maxLeft = Math.max(maxLeft, heights[left]);
+            maxLeft = Math.max(maxLeft, heights[left]); //Tomar el valor más alto del maxLeft, por si es mayor.
             total += maxLeft - heights[left];
             left += 1;
         } else {
+            /* Mismo caso , entramos cuando heights[right] < heights[left], por 
+            ende maxRight es menor que maxLeft */
             maxRight = Math.max(maxRight, heights[right]);
-            total += maxRight -heights[right];
+            total += maxRight - heights[right];
             right -= 1;
         }
     }
