b2d2e5a85eda8ddb287742eb0427d62b2fdcfa0c
Seldon <daanmaro99june@gmail.com>
2025-10-15 13:30:53 -0600

    refactor(squareRoot): Rename function to mySqrt and update right initialization logic

--- roadMapNeetCode/binarySearch/squareRoot69.js
+++ roadMapNeetCode/binarySearch/squareRoot69.js
@@ -2,9 +2,19 @@
     69: Square root of a number:
 
 */
-var squareRoot = function(x) {
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
     let left = 0;
-    let right = x;
+    let right = x === 1 || x === 0 ? x : x -1
+    /* 
+     right = x === 1 || x === 0 ? x : x -1
+     That's also works, the point, if we don't have a square root equal
+     to x, except for 0 or 1 cases, for that, before
+     we had that ternary, instead right = x, works in all cases.
+     */
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
@@ -18,7 +28,7 @@ var squareRoot = function(x) {
         }
     }
     return right
-}
+};
 
 
 console.debug(squareRoot(4));
