b702313e45f87dbf5a31f5538e1aac5ed1e6f3bf
Seldon <daanmaro99june@gmail.com>
2025-10-28 10:54:15 -0600

    feat(rContainerWithMostWater): Implement maxArea function to calculate the maximum water container area

---
+++ freeCallSecond/rContainerWithMostWater.js
@@ -0,0 +1,41 @@
+/* 
+    11: Container with most water
+    This is a review, previously I had resolved. so let's see:
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ /* Formula
+    area = Min(height[left], height[right]) * base
+    base = right - left;
+  */
+var maxArea = function(height) {
+    let baseArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    console.debug(left, right);
+    while(left < right){
+
+        //Compute the area
+        let base = right - left;
+        let area = Math.min(height[left], height[right]) * base;
+        console.debug('Area', area);
+        
+        if(area > baseArea){
+            baseArea = area;
+        }
+
+        //Moves pointers
+        if(height[left] < height[right]){
+            left += 1
+        }{
+            right -= 1
+        }
+    }
+
+    return baseArea;
+};
+
+console.debug(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
