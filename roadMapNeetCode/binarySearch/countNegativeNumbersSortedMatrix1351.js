80ae5be4bb9c73291861cafd17ed46be070ee6ed
Seldon <daanmaro99june@gmail.com>
2025-11-05 11:42:27 -0600

    feat(countNegativeNumbers): Implement function to count negative numbers in a sorted matrix with debug examples

---
+++ roadMapNeetCode/binarySearch/countNegativeNumbersSortedMatrix1351.js
@@ -0,0 +1,55 @@
+/* 
+    1351: Count Negative Numbers in a Sorted Matrix
+
+    Own hints:
+
+*/
+
+var countNegatives = function (grid) {
+    let totalCount = 0
+    
+    for (const row of grid) {
+        let firstNegative = binarySearchFirstNegative(row);
+
+        let totalNegativeRow = row.length - firstNegative;
+
+        totalCount += totalNegativeRow;
+
+    }
+    return totalCount;
+}
+
+function binarySearchFirstNegative(row) {
+    let left = 0
+    let right = row.length - 1
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+        if (row[mid] < 0) {
+            right = mid;
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+console.debug(binarySearchFirstNegative([-1,-1,-2,-3]));
+console.debug(binarySearchFirstNegative([4,3,2,-1]));
+
+
+
+console.debug(countNegatives([
+    [4,3,2,-1],
+    [3,2,1,-1],
+    [1,1,-1,-2],
+    [-1,-1,-2,-3],
+]));
+console.debug(countNegatives([
+    [3,2],
+    [1,0]
+]));
+console.debug(countNegatives([
+    [1,-1],
+    [-1,-1]
+]));
