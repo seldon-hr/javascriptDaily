7c19f39f569e6283b2d4e82f507a61a7748c013d
Seldon <daanmaro99june@gmail.com>
2025-11-13 10:21:53 -0600

    fix(longestSubstring): Understanding the pattern.

--- roadMapNeetCode/Sliding-Window/3longestSubstringWithoutRepeatingCharacters.js
+++ roadMapNeetCode/Sliding-Window/3longestSubstringWithoutRepeatingCharacters.js
@@ -1,3 +1,7 @@
+ /*
+    The key here is to know how the sliding window is going to be expanding.
+For this, two nested loops, for and while; right almost always advanced (for), while left advanced in special cases (while). 
+ */
 var longestSubstring = function (s) {
     s = s.split('')
     let mapExisting = new Set()
@@ -5,23 +9,23 @@ var longestSubstring = function (s) {
     let maxLength = 0;
     
     for (let right = 0; right < s.length; right++) {
-        //Expanding window by adding s[right]
-        mapExisting.add(s[right])
-
-        while (left < s.length && s[left] === s[right]) {
-
-            if (mapExisting.get(s[right])) {
-                mapExisting.delete(s[left]) //it's the same
-            }
-
+        //While s[right] is already in the window(duplicate)
+        
+        while (mapExisting.has(s[right])) {
+            mapExisting.delete(s[left]) //it's the same 
             left += 1
         }
+
+        //Add s[right] it wasn't a duplicate
+        mapExisting.add(s[right])
         
         //Update maxLength
-        let currentLength = right - left
+        let currentLength = right - left + 1 //discount where's start to count.
         maxLength = Math.max(maxLength, currentLength)      
     }   
     return maxLength
 }
 
 console.debug(longestSubstring('abcabcbb'));
+console.log(longestSubstring("bbbbb"));   
+console.log(longestSubstring("pwwkew"));
