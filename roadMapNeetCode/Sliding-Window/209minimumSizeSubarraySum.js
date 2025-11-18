714d1781815969d734c672cbdcef7a9fe60a4d08
Seldon <daanmaro99june@gmail.com>
2025-11-18 14:38:18 -0600

    feat(minimumSize): Implement minimum size subarray sum function

---
+++ roadMapNeetCode/Sliding-Window/209minimumSizeSubarraySum.js
@@ -0,0 +1,26 @@
+
+var minimumSize = function (nums, target) {
+    let left = 0
+    let currentSum = 0;
+    let minLength = Infinity
+
+    for (let right = 0; right < nums.length; right++) {
+        //Add  nums[right] to window
+        currentSum += nums[right]
+
+        //Valid window.
+        while (currentSum >= target) {
+            //Try to minimize: update minLength
+
+            minLength = Math.min(minLength, )
+
+            currentSum -= nums[left]
+            left -= 1
+        }
+
+    }
+    return minLength === Infinity ? 0 : minLength
+}
+
+
+console.debug(minimumSize([2,3,1,2,4,3],7));
