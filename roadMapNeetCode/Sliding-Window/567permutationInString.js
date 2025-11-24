3d26c0fcc88d2f9c87a7e222e5755fecd92449f1
Seldon <daanmaro99june@gmail.com>
2025-11-24 12:44:15 -0600

    refactor(permutation): Move mapTemporal initialization inside the loop for better scope management

--- roadMapNeetCode/Sliding-Window/567permutationInString.js
+++ roadMapNeetCode/Sliding-Window/567permutationInString.js
@@ -16,16 +16,24 @@ var isTherePermutations = function (s1, s2) {
     let left = 0
     /* hint, use just one pointer, when we have a length that not change, this is the
        window size = s1.length */
-    let mapTemporal = new Map()
     for (let right = s1.length - 1; right < s2.length; right++) {
+        let mapTemporal = new Map()
         //.         ⬆️ starts at the s1.length.
-        console.debug(`Window at position [${left},${right}]`);
-
-        //Here's need check this value are in this window.
-        //1. Add the value to the mapTemporal
-            mapTemporal.set(s2[right], (mapTemporal.get(s2[right]) || 0) + 1)
+     /*    console.debug(`Window at position [${left},${right}]`); */
+        
+        //Here's need check this value are in this window.<<<
+        //1. Add the values of window to the mapTemporal
+        let counter = 0;
+        while (counter < right) {
+            mapTemporal.set(s2[counter], (mapTemporal.get(s2[counter]) || 0) + 1)
+            counter += 1
+        }
+        console.debug(`Window at position [${left},${right}], Map of the current window ->`);
+        console.debug(mapTemporal);
         //2. Check if match the numbers for each one.
-            
+        
+        //3. delete the previous
+        mapTemporal.delete(s2[left])
         left += 1
     }
     
