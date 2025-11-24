2be5350dff3f6dfaeae0cf10081194aa10e4f6fb
Seldon <daanmaro99june@gmail.com>
2025-11-24 11:56:04 -0600

    refactor(permutation): Clean up code structure and remove unused logic in permutation checking function

--- roadMapNeetCode/Sliding-Window/567permutationInString.js
+++ roadMapNeetCode/Sliding-Window/567permutationInString.js
@@ -3,6 +3,7 @@ var isTherePermutations = function (s1, s2) {
     s2 = s2.split('')
 
     let left = 0
+    let right = s1.length - 1
     let tracker = new Map()
 
     //Track each case of its appears of s1
@@ -11,22 +12,10 @@ var isTherePermutations = function (s1, s2) {
         tracker.set(s1[counter],( tracker.get(s1[counter]) || 0) + 1)
     }
     
+    
 
-    for (let right = s1.length - 1; right < s2.length; right++) {
-        //Add the s2[right] to window
-
-        while (/* while s1.length quepa dentro de la window. */) {
-
-            if (condition) {
-                return true
-            } else {
-                //Remove the s2[left]
-                left += 1
-            }
-
-        }
-    }
-    return false
+    return tracker
 }
 
-console.debug(isTherePermutations('abca', 'abdc'));
+console.debug(isTherePermutations('aba', 'eidbaooo'));
+console.debug(isTherePermutations('ab', 'eidboaoo'));
