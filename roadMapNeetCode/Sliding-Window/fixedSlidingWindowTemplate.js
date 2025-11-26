64a8adf6e0107456f22c8f5b5ac58eb396ada193
Seldon <daanmaro99june@gmail.com>
2025-11-26 10:38:09 -0600

    refactor(sliding-window): Fix syntax for map updates in fixedWindowTemplate function

--- roadMapNeetCode/Sliding-Window/fixedSlidingWindowTemplate.js
+++ roadMapNeetCode/Sliding-Window/fixedSlidingWindowTemplate.js
@@ -5,14 +5,14 @@ var fixedWindowTemplate = function (s1, s2) {
     //Store the mapBase
     let mapBase = new Map()
     for (let count = 0; count < s1.length; count++) {
-       mapBase.set(s1[count], mapBase.get(s1[count]) || 0 ) + 1
+       mapBase.set(s1[count], (mapBase.get(s1[count]) || 0 ) + 1)
     }
 
     //Fill the window with first elements
     let mapTemp = new Map()
     let counter = 0
     while (counter < s2.length - 1) {
-        mapTemp.set(s2[counter], mapTemp.get(s2[counter]) || 0) + 1
+        mapTemp.set(s2[counter], (mapTemp.get(s2[counter]) || 0) + 1)
         counter += 1
     }
 
@@ -20,7 +20,7 @@ var fixedWindowTemplate = function (s1, s2) {
     let left = 0
     for (let right = s1.length - 1; right < s2.length; right++) {
         //Add the value
-        mapTemp.set(s2[right], mapTemp.get(s2[right]) || 0) + 1
+        mapTemp.set(s2[right], (mapTemp.get(s2[right]) || 0) + 1)
         
         //Compare
         if (compareMaps(mapBase, mapTemp)) {
