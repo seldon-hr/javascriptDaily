d8a566b66d118f37258cf1c8161ff73b4a4e055d
Seldon <daanmaro99june@gmail.com>
2025-12-04 10:16:50 -0600

    fix(mergeTwoSortedArrays): Consdider the cases when one of arrays was already taken. So consider the next elements.

--- roadMapNeetCode/binarySearch/meidanOfTwoSortedArrays.js
+++ roadMapNeetCode/binarySearch/meidanOfTwoSortedArrays.js
@@ -3,14 +3,30 @@ var mergeM = function (nums1, nums2) {
     let n1 = 0
     let n2 = 0
     while (n1 < nums1.length || n2 < nums2.length) {
-        console.debug('entry');
-        if (nums1[n1] <= nums2[n2]) {
-            merge.push(nums1[n1])
-            n1 += 1
+
+        //Consider when the elments are smaller, don't compare undefind
+        if (n1 >= nums1.length) {
+            //nums1 alreayd taken, n1 = undefined, just take nums2
+            while (n2 < nums2.length) {
+                merge.push(nums2[n2])
+                n2 += 1
+            }
+        } else if (n2 >= nums2.length) {
+            //nums2 alreayd taken, n2 = undefined, justa take nums1
+            while (n1 < nums1.length) {
+                merge.push(nums1[n1])
+                n1 += 1
+            }
         } else {
-            merge.push(nums2[n2])
-            n2 += 1
-        }
+            //Compare them
+             if (nums1[n1] <= nums2[n2]) {
+                merge.push(nums1[n1])
+                n1 += 1
+            } else {
+                merge.push(nums2[n2])
+                n2 += 1
+            }
+        }  
     }
     return merge
 }
